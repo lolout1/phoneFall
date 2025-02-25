ee7368ff7e1062af4c737a271c37c3ecb6e85a9a
Abheek Pradhan <appradhann@gmail.com>
2025-02-25 08:17:48 -0600

    v2
    v2

--- MyFallDetectionAppLiteRTPro/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    // We declare the versions of these plugins in libs.versions.toml
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-}
-
-// You can put any other top-level Gradle configurations here if needed.
-// Usually, for a simple single-module app, we leave this empty or minimal.
